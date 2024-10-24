906200b0b11000e015e5fb23d0113fee3356ec90
Mário Nobre <m.nobre@ymail.com>
2024-10-24 23:14:25 +0100

    appserv

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->usePublicPath(__DIR__.'/../../public_html');
+        // $this->app->usePublicPath(__DIR__.'/../../public_html');
         /* old version code
         $this->app->bind('path.public', fn() => base_path() . '/public_html';);
         */
